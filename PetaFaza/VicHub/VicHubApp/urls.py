394d5e7c29633f031ea59af48f43dd7f80ba25d2
Mihailo2903 <tm190185d@student.etf.bg.ac.rs>
2022-05-20 21:20:30 +0200

    addjoke

--- PetaFaza/VicHub/VicHubApp/urls.py
+++ PetaFaza/VicHub/VicHubApp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', index, name='home'),
     path('login/', login_req, name='login'),
     path('logout/', logout_req, name='logout'),
+    path('addjoke/', add_joke, name='add_joke'),
 ]
