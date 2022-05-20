e0449d10597dc9547aa31b8057019463528a3622
vukasin <vukasin.dragovic5@gmail.com>
2022-05-20 21:18:28 +0200

    path begginging phase

---
+++ PetaFaza/VicHub/VicHubApp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', index, name='home'),
+    path('login/', login_req, name='login'),
+    path('logout/', logout_req, name='logout'),
+]
