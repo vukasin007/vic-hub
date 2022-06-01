3984b74ac4773b4877c5238fa8c1313b98ded65e
vukasin <vukasin.dragovic5@gmail.com>
2022-06-01 18:51:08 +0200

    biranje vica gotovo

--- PetaFaza/VicHub/VicHubApp/tasks.py
+++ PetaFaza/VicHub/VicHubApp/tasks.py
@@ -23,48 +23,42 @@ def schedule_mail():
     email_from = EMAIL_HOST_USER
     # pronadji najbolji vic
     try:
-        print("1\n")
-        # ocene = Grade.objects.filter(date__gt=(datetime.datetime.now() - datetime.timedelta(days=7))) ne razumem zasto ne radi
-        ocene = Grade.objects.filter(was_reviewed='N')
-        print("1\n")
-        for oc in ocene:
-            print(oc+"\n")
+        ocene = Grade.objects.all()
         adekvatne_ocene = []
-        print("2\n")
         for ocena in ocene:
-            ocena.was_reviewed = "Y"
+            # ocena.was_reviewed = "Y"
             ocena.save()
-            if ocena.id_joke.status == "B":
+            if ocena.id_joke.status == "A":
                 adekvatne_ocene.append(ocena)
-        print("3\n")
         num_ocena = dict()  # key: id vica, value: broj ocena
         sum_ocena = dict()  # slicno
         for ocena in adekvatne_ocene:
-            id_vica = ocena.id_joke
+            id_vica = ocena.id_joke.id_joke
             num = num_ocena.get(id_vica, 0)
             num_ocena[id_vica] = num + 1
             suma = sum_ocena.get(id_vica, 0)
-            sum_ocena[id_vica] = suma + 1
-        print("4\n")
-        avg_dict = dict()
+            sum_ocena[id_vica] = suma + ocena.grade
         max = -1
         target_joke = -1
         for key in num_ocena:
-            avg_dict[key] = sum_ocena[key] / num_ocena[key]
-            if max < avg_dict[key]:
-                max = avg_dict[key]
+            if max < (sum_ocena[key] / num_ocena[key]):
+                max = sum_ocena[key] / num_ocena[key]
                 target_joke = key
-        print("5\n")
-        if target_joke == -1:
+        if max == -1:
+            print("exception\n")
             raise Exception("nepar")
-        mail_message = Joke.objects.get(target_joke).title + "\n" + Joke.objects.get(target_joke).content
-        print("6\n")
+        print(target_joke)
+        mail_message = Joke.objects.get(pk=target_joke).title + "\n\n-" + Joke.objects.get(pk=target_joke).content
+        myjoke = Joke.objects.get(pk=target_joke)
+        myjoke.status = "B"
+        myjoke.save()
+        print(mail_message)
     except:
-        mail_message = "Kako se zove vepar sa tri noge?\n\n\tVepar\n\t\t\t-admini"
+        mail_message = "Vepar\n\nKako se zove vepar sa tri noge?\n-Vepar"
 
-    # for to_email in all_subscribed:
-    #     mail_message = f'Zdravo {to_email.username}, ovo nedeljni vic je:\n' + mail_message
-    #     recipient_list = [to_email.email]
-    #     send_mail(mail_subject, mail_message, email_from, recipient_list)
-    #     print("sent email to " + to_email.username)
+    for to_email in all_subscribed:
+        message = f'Zdravo {to_email.username}, ovonedeljni vic je:\n\n {mail_message}'
+        recipient_list = [to_email.email]
+        send_mail(mail_subject, message, email_from, recipient_list)
+        print("sent email to " + to_email.username)
     return "Email sending finished ok"
